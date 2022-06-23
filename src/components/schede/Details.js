f7d522f24f18fbadd91334afcbd99ef7e173caf3
Diego Betto <loi.studios@gmail.com>
2022-06-23 17:41:01 +0200

    aggiunta api

--- src/components/schede/Details.js
+++ src/components/schede/Details.js
@@ -1,15 +1,114 @@
+import { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 
 const Details = () => {
     let {id} = useParams()
 
-    // carica i contenuti della scheda numero id
+    const [state, setState] = useState({})
 
-    return <div className="schede__details">
+    useEffect(() => {
+        // fetch('http://localhost:3000/api/schede/' + id + '.json')
+        fetch(`http://localhost:3000/api/schede/${id}.json`)
+            .then(result => result.json())
+            .then(result => setState(result))
+    }, [id])
+
+    return <div className="schede__details text-start">
         <h1>Dettagli scheda {id}</h1>
 
-        <div>
-            Contenuto della scheda
+        <div className="row">
+            <div className='col-md-6'>
+                <table className='table'>
+                    <tbody>
+                        <tr>
+                            <td><b>ID</b></td>
+                            <td>{state.id}</td>
+                        </tr>
+                        <tr>
+                            <td><b>ODV</b></td>
+                            <td>{state.odv}</td>
+                        </tr>
+                        <tr>
+                            <td><b>ODP</b></td>
+                            <td>{state.odp}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Colata</b></td>
+                            <td>{state.colata}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className='col-md-6'>
+                <table className='table'>
+                    <tbody>
+                        <tr>
+                            <td><b>Set lavorazione</b></td>
+                            <td>{state.set_lavorazione}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Qualità</b></td>
+                            <td>{state.qualita}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Tipo prodotto</b></td>
+                            <td>{state.tipo_prodotto}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        {/* riga con la form */}
+        <div className="row">
+            <div className="col">
+
+                <form className="row g-3">
+                    <div className="col-md-6">
+                        <label htmlFor="inputEmail4" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="inputEmail4" />
+                    </div>
+                    <div className="col-md-6">
+                        <label htmlFor="inputPassword4" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="inputPassword4" />
+                    </div>
+                    <div className="col-12">
+                        <label htmlFor="inputAddress" className="form-label">Address</label>
+                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+                    </div>
+                    <div className="col-12">
+                        <label htmlFor="inputAddress2" className="form-label">Address 2</label>
+                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+                    </div>
+                    <div className="col-md-6">
+                        <label htmlFor="inputCity" className="form-label">City</label>
+                        <input type="text" className="form-control" id="inputCity" />
+                    </div>
+                    <div className="col-md-4">
+                        <label htmlFor="inputState" className="form-label">State</label>
+                        <select id="inputState" className="form-select">
+                        <option>Choose...</option>
+                        <option>...</option>
+                        </select>
+                    </div>
+                    <div className="col-md-2">
+                        <label htmlFor="inputZip" className="form-label">Zip</label>
+                        <input type="text" className="form-control" id="inputZip" />
+                    </div>
+                    <div className="col-12">
+                        <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="gridCheck" />
+                        <label className="form-check-label" htmlFor="gridCheck">
+                            Check me out
+                        </label>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <button type="submit" className="btn btn-primary">Sign in</button>
+                    </div>
+                    </form>
+
+            </div>
         </div>
     </div>
 }
--- src/components/schede/List.js
+++ src/components/schede/List.js
@@ -1,34 +1,51 @@
+import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 
 const List = () => {
     const navigate = useNavigate()
+    const [rows, setRows] = useState([])
 
-    // carica i contenuti da visualizzare nella tabella
+    useEffect( () => {
+        fetch('http://localhost:3000/api/schede.json')
+        .then(result => result.json())
+        .then(result => setRows(result))
+    }, [])
 
     return <div className="schede__list">
         <h1>Elenco schede</h1>
 
-        <table>
+        <table className="table table-striped table-hover">
             <thead>
                 <tr>
                     <th>ID</th>
-                    <th>Nome</th>
+                    <th>odp</th>
+                    <th>colata</th>
+                    <th>odv</th>
+                    <th>set lavorazione</th>
+                    <th>qualità</th>
+                    <th>tipo prodotto</th>
                 </tr>
             </thead>
 
             <tbody>
-                <tr
-                    onClick={() => navigate("/schede/0")}
-                >
-                    <td>0</td>
-                    <td>Pinco</td>
-                </tr>
-                <tr
-                    onClick={() => navigate("/schede/1")}
-                >
-                    <td>1</td>
-                    <td>Pallino</td>
-                </tr>
+                {
+                    rows.map(row => (
+                    <tr 
+                        key={row.id}
+                        onClick={() => navigate(`/schede/${row.id}`)}
+                    >
+                            <td>{row.id}</td>
+                            <td>{row.odp}</td>
+                            <td>{row.colata}</td>
+                            <td>{row.odv}</td>
+                            <td>{row.set_lavorazione}</td>
+                            <td>{row.qualita}</td>
+                            <td>{row.tipo_prodotto}</td>
+                        </tr>
+                        )
+                    )
+                }
+                
             </tbody>
         </table>
     </div>
